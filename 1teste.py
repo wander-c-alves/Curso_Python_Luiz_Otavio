16ae8b5baa133876a99e7ef4f2f5f2af6c6ddafa
wander-c-alves <wander.alves.dev@gmail.com>
2026-01-12 20:29:40 -0300

    novo artquivo do curso

--- 1teste.py
+++ 1teste.py
@@ -1,24 +1,26 @@
-num1 = float(input('Digite o primeiro numero: '))
-operador = input('Digite o operador ( + - * / ): ')
-num2 = float(input('Digite o segundo numero: '))
+try:
+    num1 = float(input('Digite o primeiro número: '))
+    operador = input('Digite o operador( + - * / ): ')
+    num2 = float(input('Digite o segundo numero: '))
 
-if operador == '+':
-    resultado = num1 + num2
-    print(f'O resultado da soma é: {resultado}')
-elif operador == '-':
-    resultado = num1 - num2
-    print(f'O resultado da subtração é: {resultado}')
-elif operador == '*':
-    resultado = num1 * num2
-    print(f'O resultado da multiplicação é: {resultado}')
-elif operador == '/':
-    if num2 != 0:
-        resultado = num1 / num2
-        print(f'O resultado da divisão é: {resultado}')
+    if operador == '+':
+        resultado = num1 + num2
+        print(f'O resultado da soma é: {resultado}')
+    elif operador == '-':
+        resultado = num1-num2
+        print(f'O resultado da subtração é: {resultado}')
+    elif operador == '*':
+        resultado = num1*num2
+        print(f'Oresultado da multiplicação é: {resultado}')
+    elif operador == '/':
+        if num2 != 0:
+            resultado = num1/num2
+            print(f'O resultado da divisão é: {resultado}')
+        else:
+            print('Erro, não é possivel dividir por zero!!!')
     else:
-        print("Erro: Não é possível dividir por zero.")
-else:
-    print('Operador inválido!')
+        print('Você não digitou um caractere valido')
+
+except ValueError:
+    print('Um ou ambos os números digitados são inválidos.')
     
-
-
