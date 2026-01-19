aefe789b2195fd69e1d5a9b4a576bdfb0ee805ef
WANDERLEY COSTA ALVES <wander.alves.dev@gmail.com>
2026-01-19 20:42:20 -0300

    atualização

--- aula53_90_enumerate_para_enumerar_valores_de_iteraveis_pegar_indices.py
+++ aula53_90_enumerate_para_enumerar_valores_de_iteraveis_pegar_indices.py
@@ -3,11 +3,17 @@ enumerate - enumera iteraveis (indices)
 
 '''
 
+# lista = ['Maria', 'Helena', 'Luiz']
+# lista.append('joao')
+
+# lista_enumerada = enumerate(lista)
+# for item in lista_enumerada:
+#     print(item)
+    
 lista = ['Maria', 'Helena', 'Luiz']
 lista.append('joao')
 
-lista_enumerada = enumerate(lista)
-for item in lista_enumerada:
-    print(item)
+for indice, nome in enumerate(lista):
+    print(indice, nome,)
     
     
