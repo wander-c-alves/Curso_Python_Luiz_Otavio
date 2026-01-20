55c831c6c8a82ccf614b2668e78fc1d2c26b0d4f
WANDERLEY COSTA ALVES <wander.alves.dev@gmail.com>
2026-01-19 21:06:09 -0300

    exercicio

---
+++ aula54_91_92_Exercicio_crie_uma_lista_de_compras_com_listas.py
@@ -0,0 +1,12 @@
+'''
+Faça uma lista de compras com listas, O usuario deve ter a 
+possibilidade de inserir, apagar e listar valores da sua lista.
+Não permita que o programa quebre com erros de indices inexistentes na lista.
+
+'''
+lista = []
+lista = input('Selecione uma opção: ')
+opcao = input('[i]nserir [a]pagar [listar]')
+
+
+
