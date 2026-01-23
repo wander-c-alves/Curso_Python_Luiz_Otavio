34bc5c0e3bde9ffd0af0f225c9da1ef99e0e9a2d
WANDERLEY COSTA ALVES <wander.alves.dev@gmail.com>
2026-01-22 22:10:53 -0300

    att

--- aula61_99_100_Exercicio_gerar_o_primeiro_digito_de_um_cpf_com_python.py
+++ aula61_99_100_Exercicio_gerar_o_primeiro_digito_de_um_cpf_com_python.py
@@ -24,6 +24,21 @@ contrário disso:
 O primeiro dígito do CPF é 7
 """
 
-cpf = 74682489070
+cpf = '74682489070'
+nove_digitos = cpf[:9]
+contador_regressivo_1 = 10
 
+resultado_d_1 = 0
+for digito in nove_digitos:
+    resultado_d_1+= int(digito) * contador_regressivo_1
+    contador_regressivo_1 -=1
+digito_1 = (resultado_d_1 *10) % 11
 
+# digito_1 = digito_1 if digito_1 <=9 else 0
+if digito_1 > 9:
+    digito_1 = 0
+else:
+    digito_1=digito_1
+   
+print(digito_1)
+    
