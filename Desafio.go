35f86c6620f728aad8167a58b17b41f0d2fdd2a7
WANDERLEY COSTA ALVES <wander.alves.dev@gmail.com>
2026-03-14 12:02:09 -0300

    att

---
+++ Desafio.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+const pontoEbK = 373
+const ebuliçãoC = pontoEbK - 273
+
+func main() {
+
+	fmt.Printf("A temperatura de ebulição em Celsius é: %v°C\n", ebuliçãoC)
+}
